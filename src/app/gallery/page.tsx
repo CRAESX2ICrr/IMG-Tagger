6e990294f4c895efb01b343ec14622be4ee536a5
Ethan D'Costa <ethanmdcosta@gmail.com>
2025-10-08 23:35:28 +0530

    Cleaned up the UI and added Gallery page which will house the images uploaded by the user for tagging

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,9 @@
+const GalleryPage = () => {
+  return (
+    <section className="">
+      <h1 className="text-center text-4xl">Gallery</h1>
+      <div className="grid"></div>
+    </section>
+  );
+};
+export default GalleryPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,58 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
-import { FileUpIcon } from "lucide-react";
+import { ArrowRightIcon, FileUpIcon } from "lucide-react";
+import { ChangeEvent, useRef } from "react";
+import { useRouter } from "next/navigation";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 export default function Home() {
+  const router = useRouter();
+  const fileRef = useRef<HTMLInputElement>(null);
+
+  const handleUploadClick = () => {
+    fileRef.current?.click();
+  };
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      toast.success(`${file.name} uploaded successfully!`);
+    }
+  };
   return (
     <div className="font-sans flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <ThemeToggle />
       <div className="flex flex-col items-center justify-items-center">
-        <p className="mb-3">Click the button below to upload an image </p>
-        <Button
-          className="cursor-pointer"
-          type="button"
-          onClick={() => toast.success("Image Uploaded successfully!")}
-        >
-          <FileUpIcon />
-          Click Me
-        </Button>
+        <h1 className="text-5xl sm:text-2xl mb-3">
+          Upload Images to the gallery to get it tagged{" "}
+        </h1>
+        <div className="text-center">
+          <Label className="mb-2">Upload Image</Label>
+          <div className="grid grid-cols-2 gap-x-2">
+            <Input
+              ref={fileRef}
+              type="file"
+              multiple
+              placeholder=""
+              className="hidden"
+              onChange={handleFileChange}
+            />
+            <Button
+              variant="secondary"
+              onClick={handleUploadClick}
+              className="rounded-2xl cursor-pointer"
+            >
+              <FileUpIcon /> Choose Image
+            </Button>
+            <Button
+              className="rounded-2xl cursor-pointer"
+              onClick={() => router.push("/gallery")}
+            >
+              <ArrowRightIcon /> Go to Gallery Page
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
